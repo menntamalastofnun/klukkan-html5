4c2b344e2bea030de81467883458e4126bfc19d5
thunderingtheworld <thor9001@gmail.com>
2016-01-29 15:38:37 +0000

    adding "listen to time"

--- js/les_tima.js
+++ js/les_tima.js
@@ -1,20 +1,14 @@
 
-
-
-// Load the xml file using ajax 
-$.ajax({
-    type: "GET",
-    url: "times1.xml",
-    dataType: "xml",
-    success: function (xml) {
-
-        // Parse the xml file and get data
-        var xmlDoc = $.parseXML(xml);
-        var $xml = $(xmlDoc);
-        console.log(xmlDoc);
-		console.log($xml);
-		/* $xml.find('category[name="My t"] logo').each(function () {
-            $("#news-container").append($(this).text() + "<br />");
-        }); */
-    }
+$(document).ready( function () { 
+	
+	
+	
+	// XML með texta, tíma og hljóði
+	$.get('times1.xml', function(xml){ 
+		var times = $.xml2json(xml); 
+		console.log(times.time[0].minutes);
+	});
+	
 });
+
+console.log("derp")
