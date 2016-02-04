9812c874e1dae7c59bf512474840cba4110a6839
thunderingtheworld <thor9001@gmail.com>
2016-02-04 15:02:12 +0000

    draga_tolur improved but not complete
    victory: 1st improvement over old game

--- js/draga_tolur.js
+++ js/draga_tolur.js
@@ -3,33 +3,40 @@ $(document).ready( function () {
 		revert: 'invalid', 
 		drag: function() {
 			data = $( this ).attr('data-value');
-		} 
+			$('.droppable').css("background-color", "rgba(165, 191, 168, 0.4)");
+		},
+		stop:function(ev,ui){
+			droppableColorsOff();
+		}
 	});
 	
     $( "#drop-spot-1" ).droppable({
 		accept: '#drag-item-1', 
 		drop: function( event, ui ) {
-			$( this )
-          .find( "p" )
-            .html( "Dropped!" );
+			handleDrop( this );
 		}
     });
 	
 	$( "#drop-spot-2" ).droppable({
 		accept: '#drag-item-2', 
 		drop: function( event, ui ) {
-			$( this )
-          .find( "p" )
-            .html( "Dropped!" );
+			handleDrop( this );
 		}
     });
 	
 	$( "#drop-spot-3" ).droppable({
 		accept: '#drag-item-3', 
 		drop: function( event, ui ) {
-			$( this )
-          .find( "p" )
-            .html( "Dropped!" );
+			handleDrop( this );
 		}
     });
 });
+
+function handleDrop ( target ) {
+	$( target ).find( "p" ).html( "Dropped!" );
+	droppableColorsOff();
+}
+
+function droppableColorsOff() {
+	$('.droppable').css("background-color", "rgba(0,0,0,0)");
+}
